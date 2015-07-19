9349c9cc07c642547197098d7d4bd28dc2d8258c
Keith Slaughter <slaughtk@pacbell.net>
2015-07-19 12:35:01 -0700

    Initial buildout

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  imtr <- matrix(numeric(), nrow=0, ncol=0)
+  
+  set <- function(y) {
+    x <<- y
+    imtr <<- matrix(numeric(), nrow=0, ncol=0)
+  }
+  
+  get <- function() x
+  setinverse <- function(imtrx) imtr <<- imtrx
+  getinverse <- function() imtr
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -12,4 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ivrs <- x$getinverse()
+  if(length(ivrs) > 0) {
+    message("getting cached data")
+    return(ivrs)
+  }
+  mtrxdata <- x$get()
+  ivrs <- solve(mtrxdata, ...)
+  x$setinverse(ivrs)
+  ivrs
+
 }
